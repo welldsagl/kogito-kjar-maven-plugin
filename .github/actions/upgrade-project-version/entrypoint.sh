e6b1fe0159aeecbc696e3a3e7c420c657e561005
Simone D'Avico <simone.davico@welld.ch>
2019-12-16 15:44:03 +0100

    Set new project version as environment variable

--- .github/actions/upgrade-project-version/entrypoint.sh
+++ .github/actions/upgrade-project-version/entrypoint.sh
@@ -1,22 +1,22 @@
 #!/bin/sh -l
 
 function upgrade_project_version() {
-    PROJECT_VERSION=$(cat ./pom.xml | grep "<version>" | head -1 | awk -F'>' '{ print $2 }' | sed "s/[<\/version ]//g")
+    CURRENT_PROJECT_VERSION=$(cat ./pom.xml | grep "<version>" | head -1 | awk -F'>' '{ print $2 }' | sed "s/[<\/version ]//g")
     GITHUB_SHORT_SHA=$(echo ${GITHUB_SHA} | cut -c1-8)
 
-    echo "Project version: ${PROJECT_VERSION}"
+    echo "Project version: ${CURRENT_PROJECT_VERSION}"
     echo "Github short SHA: ${GITHUB_SHORT_SHA}"
 
     case "$CI_COMMIT_REF_NAME" in
       master)
-        sed -i -e "0,/\(<version>\)/s/\(<version>.*\)\(<\/version>\)/\1-FINAL\2/" pom.xml
+        # sed -i -e "0,/\(<version>\)/s/\(<version>.*\)\(<\/version>\)/\1-FINAL\2/" pom.xml
+        echo ::set-env name=PROJECT_VERSION::${CURRENT_PROJECT_VERSION}-FINAL
         ;;
       *)
-        sed -i -e "0,/\(<version>\)/s/\(<version>.*\)\(<\/version>\)/\1-${GITHUB_SHORT_SHA}\2/" pom.xml
+        # sed -i -e "0,/\(<version>\)/s/\(<version>.*\)\(<\/version>\)/\1-${GITHUB_SHORT_SHA}\2/" pom.xml
+        echo ::set-env name=PROJECT_VERSION::${CURRENT_PROJECT_VERSION}-${GITHUB_SHORT_SHA}
         ;;
     esac
-
-    cat pom.xml
 }
 
 upgrade_project_version
